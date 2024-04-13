593693429870e49f806cb92f10a304114620aae6
a-l-mm <166948137+a-l-mm@users.noreply.github.com>
2024-04-13 16:19:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+import random
 
+def drawBoard(spots):
+    board = (f"| {spots[1]} {spots[2]} {spots[3]} |\n"
+             f"| {spots[4]} {spots[5]} {spots[6]} |\n"
+             f"| {spots[7]} {spots[8]} {spots[9]} |")
+    print(board)
+def checkSpace(position, spots):
+    return spots[position] not in {'X', 'O'}
+def checkTurn(turn):
+    if turn % 2 == 0: return 'X'
+    else: return 'O'

6868a923e3fbd21f95fa733045e74073f52ce507
a-l-mm <166948137+a-l-mm@users.noreply.github.com>
2024-04-15 16:42:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,3 +29,17 @@ def aiMove(spots):
     
     emptySpots = [position for position in range(1, 10) if checkSpace(position, spots)]
     return random.choice(emptySpots)
+
+def checkForWin(spots):
+    if (spots[1] == spots[2] == spots[3]) \
+        or (spots[4] == spots[5] == spots[6]) \
+            or (spots[7] == spots[8] == spots[9]):
+                return True
+    elif (spots[1] == spots[4] == spots[7]) \
+        or (spots[2] == spots[5] == spots[8]) \
+            or (spots[3] == spots[6] == spots[9]):
+                return True
+    elif (spots[1] == spots[5] == spots[9]) \
+        or (spots[3] == spots[5] == spots[7]):
+                return True
+    else: return False
